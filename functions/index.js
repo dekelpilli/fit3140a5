db83e635697c21c48c65b5ffd188f1f343890f63
James Pickering <jammatpic@gmail.com>
2017-10-08 14:02:50 +1100

    updates to partially deal with async issues

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,59 @@
 const functions = require('firebase-functions')
-const functions = require('firebase-admin')
+const admin = require('firebase-admin')
 
-var morseTable = {
+const morseTable = {
     'SL': 'A', 'LSSS': 'B', 'LSLS': 'C', 'LSS': 'D', 'S': 'E', 'SSLS': 'F', 'LLS': 'G', 
     'SSSS': 'H', 'SS': 'I', 'SLLL': 'J', 'LSL': 'K', 'SLSS': 'L', 'LL': 'M', 'LS': 'N', 
     'LLL': 'O', 'SLLS': 'P', 'LLSL': 'Q', 'SLS': 'R', 'SSS': 'S', 'L': 'T', 'SSL': 'U', 
     'SSSL': 'V', 'SLL': 'W', 'LSSL': 'X', 'LSLL': 'Y', 'LLSS': 'Z'
 }
 
+let currentWord = ""
+let decodedWord = ""
+
 exports.updateData = functions.database.ref('/motionCount')
     .onWrite(event => {
-        console.log("Hello from Firebase!")
+        const motionData = event.data.val()
+        // get last value
+        const keys = Object.keys(motionData)
+        console.log(motionData[keys[keys.length-1]])
 
         // getting length of motion
-        let motionLength = parseFloat(event.data.end) - parseFLoat(event.data.start)
+        let motionLength = parseFloat(motionData.end) - parseFloat(motionData.start)
         // converting to seconds
         motionLength = motionLength / 1000
 
         // if signal is a gap
-        if (event.data.type === "gap") {
-            if (motionLength >= 3.0 && motionLength <= 7.0) {
-                
-            } else if (motionLength > 7.0) {
-                
+        if (motionData.type === "gap") {
+            // end of letter
+            if (motionLength >= 3.0 && motionLength < 7.0) {
+                console.log("Short gap detected")
+
+                // only add letter if it is valid, otherwise ignore
+                if (morseTable.hasOwnProperty(currentWord)) {
+                    decodedWord += morseTable[currentWord]
+                    currentWord = ""
+                } 
+            // end of word
+            } else if (motionLength => 7.0) {
+                console.log("Long gap detected")
+
+                if (morseTable.hasOwnProperty(currentWord)) {
+                    decodedWord += morseTable[currentWord]
+                    currentWord = ""
+                }
+                return admin.database().ref("/morse").push({"word": decodedWord})
             }
-        } else if (event.data.type == "mark") {
-            if (motionLength <= 3.0) {
-                
-            } else if (motionLength > 3.0 && motionLength <= 7.0) {
-                
+        } else if (motionData.type == "mark") {
+            // short mark
+            if (motionLength < 3.0) {
+                console.log("Short mark detected")
+                currentWord += "S"
+            // long mark
+            } else if (motionLength => 3.0 && motionLength <= 7.0) {
+                console.log("Long mark detected")
+                currentWord += "L"
             }
         }
-
-
-        // return admin.database().ref("/morse").push({"word": })
+         console.log(currentWord + " " + decodedWord)
     })
--- server.js
+++ server.js
@@ -10,24 +10,43 @@ var db = admin.database()
 var ref = db.ref("/motionCount")
 
 ref.on("value", function (snapshot) {
-    console.log(snapshot)
+    console.log("new val!")
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code)
 })
 
-// S
-ref.push({end: "2", start: "1", type: "mark" })
-ref.push({ end: "3", start: "2", type: "mark" })
-ref.push({ end: "4", start: "3", type: "mark" })
-// gap between letters
-ref.push({ end: "7", start: "4", type: "gap" })
-// O
-ref.push({ end: "10", start: "7", type: "mark" })
-ref.push({ end: "13", start: "10", type: "mark" })
-ref.push({ end: "16", start: "13", type: "mark" })
-// gap between letters
-ref.push({ end: "19", start: "16"type: "gap" })
-// S
-ref.push({ end: "20", start: "19", type: "mark" })
-ref.push({ end: "21", start: "20", type: "mark" })
-ref.push({ end: "22", start: "21", type: "mark" })
+const mock = [
+    { type: "start" },
+    { end: "2", start: "1", type: "mark" },
+    { end: "3", start: "2", type: "mark" },
+    { end: "4", start: "3", type: "mark" },
+    // gap between letters
+    { end: "7", start: "4", type: "gap" },
+    // O
+    { end: "10", start: "7", type: "mark" },
+    { end: "13", start: "10", type: "mark" },
+    { end: "16", start: "13", type: "mark" },
+    // gap between letters
+    { end: "19", start: "16", type: "gap" },
+    // S
+    { end: "20", start: "19", type: "mark" },
+    { end: "21", start: "20", type: "mark" },
+    { end: "22", start: "21", type: "mark" },
+    // gap between words
+    { end: "29", start: "22", type: "gap" }
+]
+
+function test(n) {
+    if (n < mock.length - 1) {
+        setTimeout(() => {
+            ref.push(mock[n])
+            test(++n)
+        }, 0)
+    }
+}
+
+setTimeout(() => {
+    ref.push(mock[mock.length - 1])
+}, 7000)
+
+test(0)
