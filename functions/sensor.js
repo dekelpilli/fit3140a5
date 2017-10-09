5009e8130348d665b1bb8d53616a6f2244e914a5
Dekel Pilli <dekelpilli@gmail.com>
2017-10-09 21:03:11 +1100

    added motion sensor that adds gaps as well as marks to the firebase

---
+++ functions/sensor.js
@@ -0,0 +1,74 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fit3140-a5.firebaseio.com"
+});
+
+var db = admin.database()
+var ref = db.ref('/rawData')
+
+var five = require("johnny-five")
+var board = new five.Board()
+
+var timeUnit = 1000 //on time unit, in ms. Seven of these mark a word end.
+var gap = false
+
+board.on("ready", function () {
+  // interfaces for led and motion detector
+  var led = new five.Led(13)
+  var motion = new five.Motion(6)
+  // used to measure time
+  var startTime = null
+  var endTime = null
+  var latestMotion = null
+
+  // turn LED on and measure starting time
+  motion.on("motionstart", function () {
+      console.log("Start")
+      led.on()
+      startTime = new Date().getTime()
+      if(gap) {
+        ref.push({
+          start: latestMotion,
+          end: startTime,
+          type: "gap"
+        })
+      }
+      latestMotion = startTime
+      gap = false
+  })
+
+  // calculate time motion was detected using end time of motion
+  motion.on("motionend", function () {
+      console.log("End")
+      led.off()
+      endTime = new Date().getTime()
+      latestMotion = endTime      
+      if (startTime != null) {
+        //add to firebase
+            ref.push({
+              start: startTime,
+              end: endTime,
+              type: "mark"
+            })
+            startTime = null
+            endTime = null
+          }
+  })
+  motion.on("data", function(data) {
+    //gaps can only occur while there isn't a mark motion going
+    if(!(latestMotion === null) && startTime === null && gap === false) { 
+      //gaps are only recorded if they are at least 3 time units long
+      if((data['timestamp']-latestMotion)>=(3*timeUnit)) {
+        gap = true  
+        console.log("short gap")
+      }
+      else {
+        gap = false
+      }
+    }
+  })  
+})
