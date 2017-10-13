33a2d811adabb890804253d388f07ca40f0d5a9b
dekelpilli <dekelpilli@gmail.com>
2017-10-13 22:46:00 +1100

    changed decoder to use winston, finished testing

--- functions/decoder.js
+++ functions/decoder.js
@@ -1,3 +1,5 @@
+var winston = require('winston')
+
 const morseTable = {
     'SL': 'A', 'LSSS': 'B', 'LSLS': 'C', 'LSS': 'D', 'S': 'E', 'SSLS': 'F', 'LLS': 'G', 
     'SSSS': 'H', 'SS': 'I', 'SLLL': 'J', 'LSL': 'K', 'SLSS': 'L', 'LL': 'M', 'LS': 'N', 
@@ -16,17 +18,14 @@ function Decoder(event, admin) {
 }
 
 Decoder.prototype.decode = function() {
-    //const allData = this._event //assumes data has alraedy undergone .val() 
     // get last value
     const keys = Object.keys(this._event)
 
-    // console.log(allData)
     const motionData = this._event[keys[keys.length-1]]
     this._event.splice(keys.length-1,1)
 
     // getting length of motion
     let motionLength = parseFloat(motionData.end) - parseFloat(motionData.start)
-    console.log(motionLength)
     // converting to seconds
     motionLength = motionLength / 1000
 
@@ -34,7 +33,7 @@ Decoder.prototype.decode = function() {
     if (motionData.type === "gap") {
         // end of letter
         if (motionLength >= 3.0 && motionLength < 7.0) {
-            console.log("Short gap detected")
+            winston.info("Short gap detected")
 
             // only add letter if it is valid, otherwise ignore
             if (morseTable.hasOwnProperty(this._currentWord)) {
@@ -43,7 +42,7 @@ Decoder.prototype.decode = function() {
             } 
         // end of word
         } else if (motionLength => 7.0) {
-            console.log("Long gap detected")
+            winston.info("Long gap detected")
 
             if (morseTable.hasOwnProperty(this._currentWord)) {
                 this._decodedWord += morseTable[this._currentWord] + " "
@@ -52,21 +51,21 @@ Decoder.prototype.decode = function() {
             if (this._admin) {
                 return this._admin.database().ref("/morse").push({"word": this._decodedWord})
             } else {
-                // console.log("DECODED WORD: " + decodedWord)
+                winston.info("DECODED WORD: " + decodedWord)
             }
         }
     } else if (motionData.type == "mark") {
         // short mark
         if (motionLength < 3.0) {
-            console.log("Short mark detected")
+            winston.info("Short mark detected")
             this._currentWord += "S"
         // long mark
         } else if (motionLength => 3.0 && motionLength <= 7.0) {
-            console.log("Long mark detected")
+            winston.info("Long mark detected")
             this._currentWord += "L"
         }
     }
-    console.log("Morse code: " + this._currentWord + ", Decoded: " + this._decodedWord)
+    winston.info("Morse code: " + this._currentWord + ", Decoded: " + this._decodedWord)
 }
 
 Decoder.prototype.decodeAll = function() {
@@ -77,57 +76,3 @@ Decoder.prototype.decodeAll = function() {
 
 
 module.exports = Decoder;
-
-/*
-module.exports = {
-    decode: function(event, admin) {
-        const allData = event.val() 
-        // get last value
-        const keys = Object.keys(allData)
-        // console.log(allData)
-        const motionData = allData[keys[keys.length-1]]
-
-        // getting length of motion
-        let motionLength = parseFloat(motionData.end) - parseFloat(motionData.start)
-        // converting to seconds
-        motionLength = motionLength / 1000
-
-        // if signal is a gap
-        if (motionData.type === "gap") {
-            // end of letter
-            if (motionLength >= 3.0 && motionLength < 7.0) {
-                console.log("Short gap detected")
-
-                // only add letter if it is valid, otherwise ignore
-                if (morseTable.hasOwnProperty(currentWord)) {
-                    decodedWord += morseTable[currentWord]
-                    currentWord = ""
-                } 
-            // end of word
-            } else if (motionLength => 7.0) {
-                console.log("Long gap detected")
-
-                if (morseTable.hasOwnProperty(currentWord)) {
-                    decodedWord += morseTable[currentWord] + " "
-                    currentWord = ""
-                }
-                if (admin) {
-                    return admin.database().ref("/morse").push({"word": decodedWord})
-                } else {
-                    // console.log("DECODED WORD: " + decodedWord)
-                }
-            }
-        } else if (motionData.type == "mark") {
-            // short mark
-            if (motionLength < 3.0) {
-                console.log("Short mark detected")
-                currentWord += "S"
-            // long mark
-            } else if (motionLength => 3.0 && motionLength <= 7.0) {
-                console.log("Long mark detected")
-                currentWord += "L"
-            }
-        }
-        console.log("Morse code: " + currentWord + ", Decoded: " + decodedWord)
-    }
-}*/
