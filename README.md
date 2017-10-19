25a6de4b49b2d1fb46710190395af3efd4aea625
Dekel Pilli <dekelpilli@gmail.com>
2017-10-19 13:23:50 +1100

    finished text to speech, updated readme

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ To run the tests, from the main folder, enter the following command:
 `mocha app.js`
 
 ### Decoding
-`index.js` reads data from from the firebase, then calls `decoder.js` to convert the data into words.
+`index.js` reads data from from the firebase, then calls `decoder.js` to convert the data into words. This is all handled via firebase functions.
 
-### Viewing
-Use `node client-side-reader.js` from the *functions* folder to view data on the firebase. This will update with any new morseDecoded pushes.
+Alternatively, you may use `auth-server.js` to do this locally, although this should not be necessary.
+
+### Viewing & Listening
+Use `node client-side-reader.js` from the *functions* folder to view data on the firebase. This will update with any new morseDecoded pushes.  
+Additionally, this will trigger `text-to-speech.js` to create .wav files and play them when you first open `cient-side-reader.js` and every time it senses new data.
 
 ### Sensor & Authentication
 To use the sensor, you must be logged in with a Monash email. To do so, run `auth-server.js` in the *functions* and load `localhost:8000`, then log in. Once you have logged in with a Monash email, the system will open up the sensor. The sensor will automatically connect to the firebase and send the motion information.  
--- functions/text-to-speech.js
+++ functions/text-to-speech.js
@@ -3,9 +3,7 @@ var fs = require('fs');
 var player = require('play-sound')(opts = {});
 
 
-
 var text_to_speech = new textToSpeech({
-    //"url": "https://gateway.watsonplatform.net/language-translator/api",
     "username": "20ee7483-ea2a-4536-b0f4-05611510ff1c",
     "password": "DZ4nzrTgscLM"
 });
@@ -24,29 +22,28 @@ function Speech() {
 Speech.prototype.play = function($item) {
     var newParams = this.params
     newParams.text = $item
-    text_to_speech.synthesize(newParams).
-        pipe(fs.createWriteStream($item+'-audio.wav'));    
-    player.play($item+'-audio.wav', function(err) {
+    var stream = text_to_speech.synthesize(newParams). //generate audio
+        pipe(fs.createWriteStream($item+'-audio.wav'));//write audio to file 
+    stream.on('finish', function () { //play file when write is done
+        player.play($item+'-audio.wav', function(err) {
         if(err) {
             throw err
         }
     })
-    }
+    })
+}
 
 Speech.prototype.playAll = function() {
-    while(this.queue.length) {
-        this.play(this.queue.shift())
+    while(this.queue.length) { //while there are items in the array
+        this.play(this.queue.shift()) //remove from array and play it
     }
 }
 
 Speech.prototype.add = function($item) {
     this.queue.push($item)
-    if(this.queue.length == 1) {
-        this.play()
+    if(this.queue.length == 1) { //if list was previously empty, start playing again
+        this.playAll()
     }
 }
-
-var a = new Speech()
-a.play("whatever")
 
 module.exports = Speech;
